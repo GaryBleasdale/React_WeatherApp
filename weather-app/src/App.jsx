4004d9718ebae9dcfd5539c3a0fc33dd651ea64a
Gary Bleasdale <gary@bleasdale.dev>
2023-03-08 17:56:41 -0300

    resolved the app being one step behind user input, and added date to the dailycard component

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -4,91 +4,82 @@ import './index.css'
 
 function App() {
   const userInput = useRef(0);
-  const [count, setCount] = useState(0)
-  const [city, setCity] = useState("London")
-  const [latLong, setLatLong] = useState([0,0])
-  const [maxTemperatures, setMaxTemperatures] = useState([])
-  const [minTemperatures, setMinTemperatures] = useState([])
-  const [precipitationProbabilities, setPrecipitationProbabilities] = useState([])
+  const [city, setCity] = useState()
   const [dailyCards, setDailyCards] = useState([])
-  
-  const clickHandler =(e)=>{
-    setCity(userInput.current.value)
-    setCount(pC => pC+1)
-    console.log(city,'inclickhandler')
-  }
-  
 
-  
-  useEffect(()=>{
-    if (city != "London") {
+
+  const clickHandler = () => {
+    let inputtedCity = userInput.current.value
+    setCity(inputtedCity)
+  }
+
+  useEffect(() => {
+    if (city != undefined){
       const requestURL = `https://us1.locationiq.com/v1/search?key=pk.91b221e896c050904a3a4780b44e416e&q=${city}&format=json`;
       fetch(requestURL)
-    .then((response) => response.json())
-    .then((data) => setLatLong([data[0]['lat'],data[0]['lon']]))
-    } else {
-      const requestURL = `https://us1.locationiq.com/v1/search?key=pk.91b221e896c050904a3a4780b44e416e&q=london&format=json`;
-      fetch(requestURL)
-    .then((response) => response.json())
-    .then((data) => setLatLong([data[0]['lat'],data[0]['lon']]))
-   }
-   console.log(latLong)
-   const latlongURL = `https://api.open-meteo.com/v1/forecast?latitude=${latLong[0]}&longitude=${latLong[1]}&timezone=auto&daily=temperature_2m_max,temperature_2m_min,precipitation_probability_max`;
-   fetch(latlongURL)
-   .then((response) => response.json())
-   .then((data) => {
-    let dailyData = [
-      {
-        'maxTemp': 0,
-        'minTemp' : 0,
-        'rainProb' : 0
-      },
-      {
-        'maxTemp': 0,
-        'minTemp' : 0,
-        'rainProb' : 0
-      },
-      {
-        'maxTemp': 0,
-        'minTemp' : 0,
-        'rainProb' : 0
-      }
-    ]
-
-    let maxDailyTemps = data['daily']['temperature_2m_max'];
-    maxDailyTemps.slice(0,3).forEach((element, index) => {
-      dailyData[index]['maxTemp'] = element
-    });
-    let minDailyTemps = data['daily']['temperature_2m_min'];
-    minDailyTemps.slice(0,3).forEach((element, index) => {
-      dailyData[index]['minTemp'] = element
-    });
-    let precipitationProbs = data['daily']['precipitation_probability_max']
-    precipitationProbs.slice(0,3).forEach((element, index) => {
-      dailyData[index]['rainProb'] = element
-    });
-    console.log(dailyData)
-    let dailyCardArray = dailyData.map((e)=>{
-       return <DailyCard maxTemperature={e['maxTemp']} minTemperature={e['minTemp']} rainProb={e['rainProb']}/>
-    })
-    setDailyCards(dailyCardArray)
-    });
-   }
-   
-   ,[count])
- 
-
-
+        .then((response) => response.json())
+        .then((data) => {
+          let lat = data[0]['lat']
+          let lon = data[0]['lon']
+          const latlongURL = `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&timezone=auto&daily=temperature_2m_max,temperature_2m_min,precipitation_probability_max`;
+          fetch(latlongURL)
+            .then((response) => response.json())
+            .then((data) => {
+              let dailyData = [
+                {
+                  'maxTemp': 0,
+                  'minTemp': 0,
+                  'rainProb': 0
+                },
+                {
+                  'maxTemp': 0,
+                  'minTemp': 0,
+                  'rainProb': 0
+                },
+                {
+                  'maxTemp': 0,
+                  'minTemp': 0,
+                  'rainProb': 0
+                }
+              ]
   
-  
+              let maxDailyTemps = data['daily']['temperature_2m_max'];
+              maxDailyTemps.slice(0, 3).forEach((element, index) => {
+                dailyData[index]['maxTemp'] = element
+              });
+              let minDailyTemps = data['daily']['temperature_2m_min'];
+              minDailyTemps.slice(0, 3).forEach((element, index) => {
+                dailyData[index]['minTemp'] = element
+              });
+              let precipitationProbs = data['daily']['precipitation_probability_max']
+              precipitationProbs.slice(0, 3).forEach((element, index) => {
+                dailyData[index]['rainProb'] = element
+              });
+              console.log(dailyData)
+              let dailyCardArray = dailyData.map((e,i) => {
+                return <DailyCard maxTemperature={e['maxTemp']} minTemperature={e['minTemp']} rainProb={e['rainProb']} day={i} />
+              })
+              setDailyCards(dailyCardArray)
+            });
+        })
+    }
+    }
+    , [city])
+
+
+
+
+
 
   return (
     <div>
       <h2>Weather App</h2>
-      <input type='text' ref={userInput} onClick={clickHandler}/>
-      <button >Check weather</button>
+      <input type='text' ref={userInput} />
+      <button onClick={clickHandler}>Check weather</button>
+      <div>Weather for {city}</div>
       <div className='cardContainer'>
-         {dailyCards}
+        {dailyCards}
+
       </div>
     </div>
   )
--- weather-app/src/DailyCard.jsx
+++ weather-app/src/DailyCard.jsx
@@ -1,6 +1,19 @@
 export default function DailyCard(props){
+    
+    let date;
+    if (props.day == 0){
+        date = 'Today'
+    }
+    if (props.day == 1){
+        date = 'Tomorrow'
+    }
+    if (props.day == 2){
+        date = 'Day after tomorrow'
+    }
+
     return (
     <div className='dailyCard'>
+        <h1> {date}</h1>
         <h1>MAX: {props.maxTemperature}C</h1>
         <h1>MIN: {props.minTemperature}C</h1>
         <h1>Chance of Rain: {props.rainProb}%</h1>
