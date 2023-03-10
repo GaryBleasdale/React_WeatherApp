22d126dff5a25ca2c6c95e13cbbe4f8fd82e0240
Gary Bleasdale <gary@bleasdale.dev>
2023-03-10 12:37:02 -0300

    minor adjustment to 3rd day date name logic, and minor adjustments to mobile responsiveness

--- weather-app/src/DailyCard.jsx
+++ weather-app/src/DailyCard.jsx
@@ -1,34 +1,39 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCloudRain, faSun } from '@fortawesome/free-solid-svg-icons'
 
-export default function DailyCard(props){
-    
-    let todaysDate = new Date()
-    console.log(todaysDate.getDay())
-    
+export default function DailyCard(props) {
+
+
 
     let date;
-    if (props.day === 0){
+    if (props.day === 0) {
         date = 'Today';
     }
-    if (props.day === 1){
+    if (props.day === 1) {
         date = 'Tomorrow';
     }
-    if (props.day === 2){
-        let days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    if (props.day === 2) {
+        let daysMap = { 
+            0: 'Tuesday', 
+            1: 'Wednesday', 
+            2: 'Thursday', 
+            3: 'Friday', 
+            4: 'Saturday',
+            5: 'Sunday', 
+            6: 'Monday',  };
         let todaysDate = new Date()
-        let index= todaysDate.getDay()
-        date = days[index+2];
-    }        
-    
+        let index = todaysDate.getDay()
+        date = daysMap[index];
+    }
+
 
     return (
         <div className='dailyCard card'>
             <h1 className='card-date'>{date}</h1>
             <div className='card-weather flex justify-center items-center my-4'>
-                {props.rainProb > 50 ? 
-                    <FontAwesomeIcon icon={ faCloudRain } className='card-icon card-icon--rain' />
-                    : <FontAwesomeIcon icon={ faSun } className='card-icon card-icon--sun' />
+                {props.rainProb > 50 ?
+                    <FontAwesomeIcon icon={faCloudRain} className='card-icon card-icon--rain' />
+                    : <FontAwesomeIcon icon={faSun} className='card-icon card-icon--sun' />
                 }
                 <div className='card-temperature'>
                     <h1 className='card-max'>MAX: <span className='card-temperature-value'>{props.maxTemperature}C</span></h1>
